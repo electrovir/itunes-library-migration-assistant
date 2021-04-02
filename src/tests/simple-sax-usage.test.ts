4117f7d5001894a2e8848d3c0bcf7a5aef0f944e
electrovir <electrovir@users.noreply.github.com>
2021-04-02 07:17:26 -0600

    add closing tag test

--- src/tests/simple-sax-usage.test.ts
+++ src/tests/simple-sax-usage.test.ts
@@ -5,6 +5,7 @@ import {getSampleFilePath} from '../file-paths';
 
 testGroup((runTest) => {
     runTest({
+        description: 'all opening tags should get counted',
         expect: 5,
         test: async () => {
             return new Promise<number>((resolve, reject) => {
@@ -29,6 +30,34 @@ testGroup((runTest) => {
             });
         },
     });
+
+    runTest({
+        description: 'all closing tags should get counted',
+        expect: 5,
+        test: async () => {
+            return new Promise<number>((resolve, reject) => {
+                const saxStream = createStream(true);
+                const nodes: string[] = [];
+                saxStream.on('closetag', (node) => {
+                    nodes.push(node);
+                });
+                saxStream.on('end', () => {
+                    resolve(nodes.length);
+                });
+                saxStream.on('error', (error) => {
+                    reject(error);
+                });
+                const stream = createReadStream(getSampleFilePath('simple-example.xml'));
+                // catch file not found errors
+                stream.on('error', (error) => {
+                    reject(error);
+                });
+
+                stream.pipe(saxStream);
+            });
+        },
+    });
+
     runTest({
         description: 'invalid file paths should cause error',
         expectError: {
